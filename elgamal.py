e36a90ed8c2a89531dfeafc94e71b0605e8c0a0f
heyjoakim <joakimhey@gmail.com>
2020-09-26 14:34:27 +0200

    progress

--- elgamal.py
+++ elgamal.py
@@ -1,20 +1,20 @@
 g = 666
 p = 6661  # generator
 
-a = 37  # Alice secret key
-b = 144  # Bob secret key
+x = 37  # Bob secret key
+y = 144  # Alice secret key
 
-A = g**a % p  # Message send to Bob
-B = g**b % p  # Message send to Alice
-print("Alice sends to Bob ", A)
-print("Bob sends to Alice", B)
+B = g**x % p  # Message send to Alice
+A = g**y % p  # Message send to Bob
+print("Bob sends (g^x mod p) to Alice", B)
+print("Alice computes (g^y mod p) ", A)
 
-s_Alice = B**a % p  # Alice computes secret
-s_Bob = A**b % p  # Bob computes secret
+s_Bob = A**x % p  # Bob computes secret
+s_Alice = B**y % p  # Alice computes secret
 
 assert s_Alice == s_Bob
-print("Alice computes the secret ", s_Alice)
-print("Bob computes the secret", s_Bob)
+#print("Alice computes (g^y mod p) ", s_Alice)
+#print("Bob computes the secret", s_Bob)
 
 # Messages intercepted
 A_message = 820
@@ -26,15 +26,15 @@ print("-"*20)
 def bruteforce(A, B):
     for i in range(1, p):
         if g**i % p == A:
-            a_key = i
+            x_key = i
         if g**i % p == B:
-            b_key = i
+            y_key = i
 
-    print("Alice secret was", a_key)
-    print("Bob secret was", b_key)
+    print("Alice secret was", x_key)
+    print("Bob secret was", y_key)
 
-    assert B**a % p == A**b % p
-    s = A**b % p
+    assert B**x_key % p == A**y_key % p
+    s = A**x_key % p
     print("Adversary computes secret", s)
 
 
