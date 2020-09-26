2d2e96f60f1892d27064c183516bef94536a5190
heyjoakim <joakimhey@gmail.com>
2020-09-26 20:27:48 +0200

    don

--- elgamal.py
+++ elgamal.py
@@ -1,42 +1,71 @@
+"""
+Alice & Bob agreed on base g = 666 with a generator p = 6661
+Bob then sends his Public-key (g^x mod p) = 2227 to Alice
+"""
 g = 666
 p = 6661  # generator
+B = 2227  # Message send to Alice
 
-x = 37  # Bob secret key
-y = 144  # Alice secret key
+print("Bob sends PK (" + str(B) + ") to alice ")
 
-B = g**x % p  # Message send to Alice
-A = g**y % p  # Message send to Bob
-print("Bob sends (g^x mod p) to Alice", B)
-print("Alice computes (g^y mod p) ", A)
+y = 144 # Alice random key
+Alice_PK = g**y % p  # Alice PK
+Key = B**y % p  # Alice computes secret
+print(Key)
+c = Key * 2000  # Alice compute her secret message c
+print(c)
 
-s_Bob = A**x % p  # Bob computes secret
-s_Alice = B**y % p  # Alice computes secret
+print("Alice sends PK with encrypted message (" + str(Alice_PK) + "," +  str(c) + ") to Bob." )
 
-assert s_Alice == s_Bob
-#print("Alice computes (g^y mod p) ", s_Alice)
-#print("Bob computes the secret", s_Bob)
+
+""" Eve intercepts Alice's message (AlicePK, c) """
 
 # Messages intercepted
-A_message = 820
-B_message = 5333
+A_message, c = (Alice_PK, c)
+B_message = B
 
-print("-"*20)
-
+print("-"*66)
+print("Eve intercepts the messages and bruteforces")
+print("-"*66)
 
 def bruteforce(A, B):
     for i in range(1, p):
         if g**i % p == A:
-            x_key = i
-        if g**i % p == B:
             y_key = i
+        if g**i % p == B:
+            x_key = i
 
-    print("Alice secret was", x_key)
-    print("Bob secret was", y_key)
+    print("Alice secret was", y_key)
+    print("Bob secret was", x_key)
 
-    assert B**x_key % p == A**y_key % p
-    s = A**x_key % p
-    print("Adversary computes secret", s)
+    try:
+        assert B**y_key % p == A**x_key % p
+        secret = B**y_key % p
+        message = c // secret
+        print("Message was", message)
+    except AssertionError:
+        print("Bruteforce failed, the shared secrets were not the same")
 
 
 bruteforce(A_message, B_message)
 
+""" 
+Mallory intercepts the messages and modifies the message to '6000'
+M knows Alice's message and her secret key and acts like Alice
+"""
+M_Key = B_message**y % p # Bob's PK, Alices secret key
+malory_c = M_Key * 6000 # Corrupting c
+modified_message = Alice_PK, malory_c  # An easier solution would be to multiply c by 3
+
+print("-"*66)
+print("Mallory intercepts and modifies the message")
+print("-"*66)
+
+print("Mallory sends Bob the message modified", modified_message)
+
+# Bob decrypting
+PK, corrupted_message = modified_message
+
+B_Key = PK**66 % p # Bob should know his secret key 66
+decrypted_message = corrupted_message // B_Key 
+print("Bob decrypts the modified message", decrypted_message) 
