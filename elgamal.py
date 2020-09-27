46b87bd0bd5d2477729c45aa6ff9013fafa0e8c3
heyjoakim <joakimhey@gmail.com>
2020-09-27 14:36:27 +0200

    donedone

--- elgamal.py
+++ elgamal.py
@@ -1,32 +1,25 @@
-"""
-Alice & Bob agreed on base g = 666 with a generator p = 6661
-Bob then sends his Public-key (g^x mod p) = 2227 to Alice
-"""
+"""Alice & Bob agreed on base g = 666 with a generator p = 6661 """
+print("--- Key exchange & Encryption ---")
 g = 666
 p = 6661  # generator
 B = 2227  # Message send to Alice
 
-print("Bob sends PK (" + str(B) + ") to alice ")
+print("Bob sends PK ({0}) to Alice".format(B)) # Bob then sends his Public-key (g^x mod p) = 2227 to Alice
 
 y = 144 # Alice random key
 Alice_PK = g**y % p  # Alice PK
 Key = B**y % p  # Alice computes secret
-print(Key)
 c = Key * 2000  # Alice compute her secret message c
-print(c)
 
-print("Alice sends PK with encrypted message (" + str(Alice_PK) + "," +  str(c) + ") to Bob." )
+print("Alice sends PK with encrypted message ({0},{1}) to Bob".format(Alice_PK, c))
 
 
 """ Eve intercepts Alice's message (AlicePK, c) """
+print(" --- Eve intercepting ---")
 
 # Messages intercepted
 A_message, c = (Alice_PK, c)
 B_message = B
-
-print("-"*66)
-print("Eve intercepts the messages and bruteforces")
-print("-"*66)
 
 def bruteforce(A, B):
     for i in range(1, p):
@@ -49,17 +42,12 @@ def bruteforce(A, B):
 
 bruteforce(A_message, B_message)
 
-""" 
-Mallory intercepts the messages and modifies the message to '6000'
-M knows Alice's message and her secret key and acts like Alice
-"""
-M_Key = B_message**y % p # Bob's PK, Alices secret key
+""" Mallory intercepts the messages and modifies the message to '6000' """
+print(" --- Mallory intercepting ---")
+
+M_Key = B_message**y % p # Secret computes with Bob's PK, Alices secret key
 malory_c = M_Key * 6000 # Corrupting c
 modified_message = Alice_PK, malory_c  # An easier solution would be to multiply c by 3
-
-print("-"*66)
-print("Mallory intercepts and modifies the message")
-print("-"*66)
 
 print("Mallory sends Bob the message modified", modified_message)
 
